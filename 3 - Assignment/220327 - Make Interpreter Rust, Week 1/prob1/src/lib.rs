98071df78edd975a86790c059901be9d5bcc0019
siontama <siontama@naver.com>
2022-05-26 14:15:24 +0900

    Solve: prob1 revive

--- 3 - Assignment/220327 - Make Interpreter Rust, Week 1/prob1/src/lib.rs
+++ 3 - Assignment/220327 - Make Interpreter Rust, Week 1/prob1/src/lib.rs
@@ -6,11 +6,41 @@ pub struct Player {
 
 impl Player {
     pub fn revive(&self) -> Option<Player> {
-        unimplemented!("Revive this player")
+        //unimplemented!("Revive this player")
+        if self.health == 0 {
+            if self.level >= 10 {
+                return Some(Player {
+                    health: 100,
+                    mana: Some(100),
+                    level: self.level,
+                });
+            } else {
+                return Some(Player {
+                    health: 100,
+                    mana: None,
+                    level: self.level,
+                });
+            }
+        } else {
+            return None;
+        }
     }
 
     pub fn cast_spell(&mut self, mana_cost: u32) -> u32 {
-        unimplemented!("Cast a spell of cost {}", mana_cost)
+        //unimplemented!("Cast a spell of cost {}", mana_cost)
+        if self.mana == None {
+            if self.health > mana_cost {
+                self.health -= mana_cost;
+            } else {
+                self.health = 0;
+            }
+            return 0;
+        } else if self.mana.unwrap() < mana_cost {
+            return 0;
+        } else {
+            self.mana = Some(self.mana.unwrap() - mana_cost);
+            return mana_cost*2;
+        }
     }
 }
 
